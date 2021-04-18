a1bc886a79cfa66fa225b7cde933f165c49ec28a
Daniel <dwsuarez@gmail.com>
2021-04-18 19:10:53 -0300

    first version

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 from tkinter import*
-from PIL import Image, ImageChops, ImageEnhance, ImageOps
-
-
-
 
 
 class mem_app:
     def __init__(self,window):
         self.root=window
         self.root.title("Scraper titulos LA NACION")
-        self.root.config(bg="#B22222")
+        self.root.config(bg="#FFFFFF")
 
 
         self.imagen=PhotoImage(file="images.png")
@@ -25,8 +21,7 @@ class mem_app:
         btn_scrap = Button(self.root,text='Scrapear Titulos',padx=5,pady=5,bg='#836F41',command=self.scrap_n)
         btn_scrap.grid(row=2,column=0)
 
-        """label_titulo1 = Label(self.root, text='Nombre:',font=('arial', 12, 'bold'),bg="#7C3E48")
-        label_titulo1.grid(row=1, column = 0, sticky='W')"""
+    
     
     def scrap_n(self):
         url = 'https://www.lanacion.com.ar/'
@@ -46,43 +41,20 @@ class mem_app:
         
         print (lst_title)    
             
-        lbl1 =  Label(self.root, text= lst_title[0],font=('arial', 12, 'bold'),bg="#7C3E48")
+        lbl1 =  Label(self.root, text= lst_title[0],font=('arial', 12, 'bold'),bg="#FFFFFF")
         lbl1.grid(row=3,column=0)
-        lbl2 =  Label(self.root, text=lst_title[1],font=('arial', 12, 'bold'),bg="#7C3E48")
+        lbl2 =  Label(self.root, text=lst_title[1],font=('arial', 12, 'bold'),bg="#FFFFFF")
         lbl2.grid(row=4,column=0)
-        lbl3 =  Label(self.root, text=lst_title[2],font=('arial', 12, 'bold'),bg="#7C3E48")
+        lbl3 =  Label(self.root, text=lst_title[2],font=('arial', 12, 'bold'),bg="#FFFFFF")
         lbl3.grid(row=5,column=0)
-        lbl4 =  Label(self.root, text=lst_title[3],font=('arial', 12, 'bold'),bg="#7C3E48")
+        lbl4 =  Label(self.root, text=lst_title[3],font=('arial', 12, 'bold'),bg="#FFFFFF")
         lbl4.grid(row=6,column=0)
-        lbl5 =  Label(self.root, text=lst_title[4],font=('arial', 12, 'bold'),bg="#7C3E48")
+        lbl5 =  Label(self.root, text=lst_title[4],font=('arial', 12, 'bold'),bg="#FFFFFF")
         lbl5.grid(row=7,column=0)
             
      
-           
-            
-        
-            
-        
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 if __name__=='__main__':
     window=Tk()
     app=mem_app(window)
     window.mainloop()
-
-
